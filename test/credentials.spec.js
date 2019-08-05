107b101e464cab98930550f872219a0527dcd448
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-08-05 09:59:21 -0700

    feat: Token file web identity credentials

--- test/credentials.spec.js
+++ test/credentials.spec.js
@@ -1380,6 +1380,57 @@
         expect(AWS.ECSCredentials).to.equal(AWS.RemoteCredentials);
       });
     });
+    describe('AWS.TokenFileWebIdentityCredentials', function() {
+      var origEnv;
+      var fs = require('fs');
+      beforeEach(function() {
+        origEnv = process.env;
+        process.env = {
+          AWS_WEB_IDENTITY_TOKEN_FILE: 'envTokenFile',
+          AWS_IAM_ROLE_ARN: 'envRoleArn',
+          ENV_ROLE_SESSION_NAME: 'envSessionName'
+        };
+        helpers.spyOn(AWS.util, 'getProfilesFromSharedConfig').andReturn(
+          {
+            'default': {
+              'web_identity_token_file': 'cfgTokenFile',
+              'role_arn': 'cfgRoleArn',
+              'role_session_name': 'cfgSessionName'
+            }
+          }
+        );
+        helpers.spyOn(fs, 'readFileSync').andReturn('oidcToken');
+      });
+      afterEach(function() {
+        iniLoader.clearCachedFiles();
+        process.env = origEnv;
+      });
+
+      it('reads params from environment variables when available', function() {
+        new AWS.TokenFileWebIdentityCredentials();
+        return expect(fs.readFileSync.calls[0]['arguments'][0]).to.equal('envTokenFile');
+      });
+
+      return describe('reads params from shared config when ones not available from environment variable', function() {
+        it('when AWS_WEB_IDENTITY_TOKEN_FILE is not available', function() {
+          delete process.env.AWS_WEB_IDENTITY_TOKEN_FILE;
+          new AWS.TokenFileWebIdentityCredentials();
+          return expect(fs.readFileSync.calls[0]['arguments'][0]).to.equal('cfgTokenFile');
+        });
+
+        it('when AWS_IAM_ROLE_ARN is not available', function() {
+          delete process.env.AWS_IAM_ROLE_ARN;
+          new AWS.TokenFileWebIdentityCredentials();
+          return expect(fs.readFileSync.calls[0]['arguments'][0]).to.equal('cfgTokenFile');
+        });
+
+        return it('when process.env is empty', function() {
+          process.env = {};
+          new AWS.TokenFileWebIdentityCredentials();
+          return expect(fs.readFileSync.calls[0]['arguments'][0]).to.equal('cfgTokenFile');
+        });
+      });
+    });
   }
 
   describe('AWS.TemporaryCredentials', function() {
